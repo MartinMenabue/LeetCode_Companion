05908fe4ac310dc9d08fd7dd8d4c09e58054099e
Martin Menabue <martinmnb@live.it>
2024-12-11 17:03:26 +0100

    Updated argument parsing

--- main.py
+++ main.py
@@ -6,43 +6,30 @@ from problem import prepare_problem, interpret_solution, submit_solution
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='LeetCode Companion')
     parser.add_argument('--verbose', '-v', action='store_true', help='Print debug information')
-    parser.add_argument('command', type=str, choices=['prepare', 'run', 'submit'], help='Command to execute')
-    args1, other1 = parser.parse_known_args()
+    subparsers = parser.add_subparsers(help='commands', dest='command', required=True)
+    prepare_parser = subparsers.add_parser('prepare', help='Prepare a problem')
+    prepare_parser.add_argument('problem_name', type=str, help='Problem name')
+    run_parser = subparsers.add_parser('run', help='Run the solution of a problem with the provided test cases')
+    run_parser.add_argument('problem_name', type=str, help='Problem name')
+    submit_parser = subparsers.add_parser('submit', help='Submit the solution of a problem')
+    submit_parser.add_argument('problem_name', type=str, help='Problem name')
+    args = parser.parse_args()
     s = requests.Session()
-    verboseprint = print if args1.verbose else lambda *a, **k: None
-    res = check_login(args1, s)
-    if args1.command == 'prepare':
-        parser = argparse.ArgumentParser(description='Prepare a problem')
-        parser.add_argument('problem_name', type=str, help='Problem name')
-        args2 = parser.parse_args(other1)
-        for arg in vars(args2):
-            setattr(args1, arg, getattr(args2, arg))
-        args = args1
+    res = check_login(args, s)
+    if args.command == 'prepare':
         pname = args.problem_name
-        verboseprint(f'Preparing problem {pname}')
+        print(f'Preparing problem {pname}')
         problem_info = prepare_problem(args, s, pname)
-    
-    if args1.command == 'run':
-        parser = argparse.ArgumentParser(description='Run the solution of a problem with the provided test cases')
-        parser.add_argument('problem_name', type=str, help='Problem name')
-        args2 = parser.parse_args(other1)
-        for arg in vars(args2):
-            setattr(args1, arg, getattr(args2, arg))
-        args = args1
+    elif args.command == 'run':
         pname = args.problem_name
-        verboseprint(f'Running problem {pname} with the provided test cases')
+        print(f'Running problem {pname} with the provided test cases')
         problem_info = interpret_solution(args, s, pname)
-
-    if args1.command == 'submit':
-        parser = argparse.ArgumentParser(description='Submit the solution of a problem')
-        parser.add_argument('problem_name', type=str, help='Problem name')
-        args2 = parser.parse_args(other1)
-        for arg in vars(args2):
-            setattr(args1, arg, getattr(args2, arg))
-        args = args1
+    elif args.command == 'submit':
         pname = args.problem_name
-        verboseprint(f'Submitting problem {pname}')
+        print(f'Submitting problem {pname}')
         submit_solution(args, s, pname)
+    else:
+        raise ValueError('Invalid command')
 
 
     
